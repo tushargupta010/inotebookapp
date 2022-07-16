c9e4f495b832b8e02dcf3addeecef8c4f4cae00b
tushar <tushargupta010@gmail.com>
2022-07-17 00:33:37 +0530

    storing data into mongodb using models

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(express.json());
+
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  obj = {
-    name: "tushar",
-    route: "auth",
-  };
-  res.json(obj);
+// Create a user using POST "/api/auth/". Doesn't require auth
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 });
 
 module.exports = router;
