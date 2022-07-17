75f3e4b10c73810de6a304f2645e88bd816272e6
tushar <tushargupta010@gmail.com>
2022-07-17 10:06:18 +0530

    securint password using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,11 +27,15 @@ router.post(
           error: "Error : user with " + req.body.email + " already exists.",
         });
       }
+
+      // securing password using bcryptjs
+      const salt = await bcrypt.genSalt(10);
+      const secPassword = await bcrypt.hash(req.body.password, salt);
       // Create a user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPassword,
       });
       // .then((user) => res.json(user))
       // .catch((err) => {
@@ -41,6 +45,7 @@ router.post(
       //     message: err.message,
       //   });
       // });
+
       res.json(user);
     } catch (error) {
       console.log(error.message);
